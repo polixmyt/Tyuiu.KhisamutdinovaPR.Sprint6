bff95b00aef481869b5737548a52893fba5686d2
Polina Khisamutdinova <polina.hisa@mail.ru>
2025-12-02 21:20:35 +0500

    Add project files.

---
+++ Tyuiu.KhisamutdinovaPR.Sprint6.Task0.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KhisamutdinovaPR.Sprint6.Task0.V1.Lib;
+
+namespace Tyuiu.KhisamutdinovaPR.Sprint6.Task0.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(3);
+            double wait = 0.103;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
