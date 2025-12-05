d88549010c62f86b506515cb0d03dc6272836099
Polina Khisamutdinova <polina.hisa@mail.ru>
2025-12-05 19:32:59 +0500

    Task2

---
+++ Tyuiu.KhisamutdinovaPR.Sprint6.Task2.V7.Lib/ISprint6Task2V7.cs
@@ -0,0 +1,6 @@
+﻿namespace Tyuiu.KhisamutdinovaPR.Sprint6.Task2.V7.Lib
+{
+    public interface ISprint6Task2V7
+    {
+    }
+}
---
+++ Tyuiu.KhisamutdinovaPR.Sprint6.Task2.V7/FormMain1.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows.Forms;
+
+namespace Tyuiu.KhisamutdinovaPR.Sprint6.Task2.V7
+{
+    internal class FormMain : Form
+    {
+    }
+}
